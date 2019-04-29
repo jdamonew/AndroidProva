58a6833c3121b631ff23d7b3346daa7628575f4d
jdamonew <41210780+jdamonew@users.noreply.github.com>
2019-04-29 16:55:27 -0300

    Teste de Activity
    indo de uma activity para outra

--- app/src/main/java/com/android/first/MainActivity.java
+++ app/src/main/java/com/android/first/MainActivity.java
@@ -2,10 +2,15 @@ package com.android.first;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button botao;
 
     //PRIMEIRO
     @Override
@@ -13,6 +18,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        botao = (Button) findViewById(R.id.btnEntrar);
+
+        botao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            startActivity(new Intent(MainActivity.this, SegundaActivity.class));
+            }
+        });
     }
     //SEGUNDO
     @Override
@@ -42,5 +55,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
     }
+
+
+//     protected void onResult(){
+//        super.onResult();
+//     }
 
     }
