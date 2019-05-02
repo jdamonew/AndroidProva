677e4929dd119e6c04edd87a115095d9af45eb49
jdamonew <41210780+jdamonew@users.noreply.github.com>
2019-05-02 14:06:55 -0300

    Tela de carregamento

---
+++ app/src/main/java/com/android/first/SplashScreen.java
@@ -0,0 +1,29 @@
+package com.android.first;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        //getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                finish();
+            }
+            //o tempo em milisegundo para executar o run - 5000 = 5sec
+        }, 3000);
+    }
+}
